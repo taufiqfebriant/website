81fff74cd91e8bde79ad8a6e68b54b0800c31c12
Muhammad Taufiq Febrianto <taufiqisfebrianto06@gmail.com>
2022-10-12 12:20:30 +0700

    Add meta tags, favicon, and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
 import image from '@astrojs/image';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		tailwind(),
-		image({
-			serviceEntryPoint: '@astrojs/image/sharp',
-		}),
-		react(),
-	],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), react(), sitemap()],
+  site: 'https://www.taufiq.co.id'
 });
